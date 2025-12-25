820015b0e09afc4d1a97f635afb5ab6d9ea4173a
Manish <raiman9705@gmail.com>
2025-12-26 00:02:31 +0530

    third commit- nav bar and intro made

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const hamburgermenu = document.querySelector('.hamburger-menu');
+const hamburgerlist = document.querySelector('.hamburger-link-container');
+hamburgermenu.addEventListener('click', ()=> {
+  hamburgerlist.classList.toggle('hidden')});  
+
+
+
